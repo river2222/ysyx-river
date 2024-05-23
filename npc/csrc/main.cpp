028c427a340515bfbdf334bccd92fa299063bdb3
tracer-ysyx <tracer@ysyx.org>
2024-05-23 12:22:52 -0400

    > sim RTL 2023234332 Yang Hongli Linux holy-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:22:52 up  5:44,  1 user,  load average: 0.29, 0.26, 0.25

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,39 @@
+#define TOP_NAME Vtop
+#include "Vtop.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "verilated.h"
+#include <Vtop.h>
+#include "verilated_vcd_c.h"
+#include <nvboard.h>
 
-int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+void nvboard_bind_all_pins(TOP_NAME* top);
+
+int main(int argc, char** argv) {
+    TOP_NAME dut; 
+    nvboard_bind_all_pins(&dut);
+    nvboard_init();
+
+    Verilated::commandArgs(argc, argv);
+    TOP_NAME* top = new TOP_NAME;
+
+
+		for (int i = 0; i < 10; i++) {
+        int a = rand() & 1;
+        int b = rand() & 1;
+        top->a = a;
+        top->b = b;
+
+        top->eval();
+        printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+        assert(top->f == (a & b));
+
+        nvboard_update();
+    }
+
+    delete top;
+
+    nvboard_quit(); 
+    exit(EXIT_SUCCESS);
 }
