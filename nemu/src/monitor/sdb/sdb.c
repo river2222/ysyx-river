00c9314c43956c23fc23b2adc463a08999d00e9b
tracer-ysyx <tracer@ysyx.org>
2024-06-10 04:26:44 -0400

    > compile NEMU 2023234332 Yang Hongli Linux holy-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  04:26:44 up 10:55,  1 user,  load average: 0.44, 0.38, 0.34

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -124,11 +124,12 @@ static int cmd_info(char *args) {
 
 static int cmd_x(char *args) {
 	int n;
-	unsigned int addr;
+	uint32_t addr;
 	sscanf(args,"%d%x",&n,&addr);
 	for (int i = 0; i < n; i ++){
 		printf("%x\n",paddr_read(addr,4));
 		addr = addr + 4;
+
 	}
 	return 0;
 }
