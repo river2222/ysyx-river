116fcc0e7d86b30a67294bd98d5bdb0e9b3ae5f5
tracer-ysyx <tracer@ysyx.org>
2024-06-10 22:59:04 -0400

    > compile NEMU 2023234332 Yang Hongli Linux holy-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:59:04 up 12:28,  1 user,  load average: 0.30, 0.32, 0.29

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -21,7 +21,7 @@
 #include <regex.h>
 
 enum {
-  TK_NOTYPE = 256, TK_EQ,
+  TK_NOTYPE = 256, TK_EQ, TK_NEQ, TK_AND, TK_OR, TK_NUM, TK_REG, TK_HEX
 
   /* TODO: Add more token types */
 
@@ -34,11 +34,24 @@ static struct rule {
 
   /* TODO: Add more rules.
    * Pay attention to the precedence level of different rules.
-   */
+
+*/
 
   {" +", TK_NOTYPE},    // spaces
+	{"\\(", '('},         // 
+	{"\\)", ')'},         //
   {"\\+", '+'},         // plus
+	{"\\-", '-'},					// minus
+	{"\\*", '*'},         // multiply
+	{"\\/", '/'},         // divide
   {"==", TK_EQ},        // equal
+	{"!=", TK_NEQ},       // not equal
+	{"\\&\\&", TK_AND},     // AND
+	{"\\|\\|", TK_OR},     // OR
+	{"[0-9]+", TK_NUM},              // integer constant
+  {"\\$(zero|ra|sp|gp|tp|t0|t1|t2|s0|s1|a[0-7]|s[2-8]|s9|s10|s11|t[3-6])", TK_REG},        // register
+  {"0[xX][0-9a-fA-F]+", TK_HEX}   // hexadecimal constant
+
 };
 
 #define NR_REGEX ARRLEN(rules)
@@ -58,8 +71,10 @@ void init_regex() {
     if (ret != 0) {
       regerror(ret, &re[i], error_msg, 128);
       panic("regex compilation failed: %s\n%s", error_msg, rules[i].regex);
-    }
-  }
+
+		}
+
+	}
 }
 
 typedef struct token {
@@ -77,7 +92,8 @@ static bool make_token(char *e) {
 
   nr_token = 0;
 
-  while (e[position] != '\0') {
+
+	while (e[position] != '\0') {
     /* Try all rules one by one. */
     for (i = 0; i < NR_REGEX; i ++) {
       if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
@@ -89,20 +105,25 @@ static bool make_token(char *e) {
 
         position += substr_len;
 
-        /* TODO: Now a new token is recognized with rules[i]. Add codes
+
+				/* TODO: Now a new token is recognized with rules[i]. Add codes
          * to record the token in the array `tokens'. For certain types
          * of tokens, some extra actions should be performed.
          */
 
-        switch (rules[i].token_type) {
+
+				switch (rules[i].token_type) {
           default: TODO();
         }
 
         break;
-      }
-    }
 
-    if (i == NR_REGEX) {
+			}
+
+		}
+
+
+		if (i == NR_REGEX) {
       printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
       return false;
     }
@@ -116,7 +137,8 @@ word_t expr(char *e, bool *success) {
   if (!make_token(e)) {
     *success = false;
     return 0;
-  }
+
+	}
 
   /* TODO: Insert codes to evaluate the expression. */
   TODO();
